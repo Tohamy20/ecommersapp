0ae0501ade156ce96d5cd8c0810b709827fc8717
Tohamy20 <mahmoed.tohamy2@gmail.com>
2023-12-20 00:27:57 +0200

    pushagainafter mahmoud

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,6 +92,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
 
 
 }
---
+++ app/src/main/java/com/example/talabatend/CaptureAct.java
@@ -0,0 +1,8 @@
+package com.example.talabatend;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity
+{
+
+}
